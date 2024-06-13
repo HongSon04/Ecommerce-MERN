5bb3051baa92b723ccc64f368214444c681f26ee
ChuBe04 <118839446+HongSon04@users.noreply.github.com>
2024-06-13 11:21:45 +0700

    add sidebar & header for admin template

--- dashboard/src/layout/Header.jsx
+++ dashboard/src/layout/Header.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React from "react";
+import { FaList } from "react-icons/fa";
 
-const Header = () => {
-    return (
-        <div>
-            <h1>Header</h1>
+const Header = ({ showSidebar, setShowSidebar }) => {
+  return (
+    <div className="fixed top-0 left-0 z-40 w-full px-2 py-5 lg:px-7">
+      <div className="ml-0 lg:ml-[260px] rounded-md h-[65px] flex justify-between items-center bg-[#b1addf] px-5 transition-all content-center">
+        {/* Icon Menu */}
+        <div
+          onClick={() => setShowSidebar(!showSidebar)}
+          className="w-[35px] flex lg:hidden h-[35px] rounded-sm shadow-indigo-500 shadow-lg hover:shadow-indigo-500/50 justify-center items-center cursor-pointer"
+        >
+          <span>
+            <FaList />
+          </span>
         </div>
-    );
+        {/* Seach */}
+        <div className="hidden md:block">
+          <input
+            className="px-3 py-2 outline-none border bg-transparent border-slate-700 rounded-md text-[#423d72] focus:border-indigo-500 overflow-hidden"
+            type="text"
+            name="search"
+            placeholder="Search..."
+          />
+        </div>
+
+        <div className="relative flex items-center justify-center gap-8">
+          <div className="flex items-center justify-center">
+            <div className="flex items-center justify-center gap-3">
+              <div className="flex flex-col items-center justify-center text-end">
+                <h2 className="font-bold text-md">Chu Be</h2>
+                <span className="text-[14px] w-full font-normal">Admin</span>
+              </div>
+              <img
+                className="w-[45px] h-[45px] rounded-full object-cover"
+                src="http://localhost:3000/images/admin.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- dashboard/src/views/admin/AdminDashboard.jsx
+++ dashboard/src/views/admin/AdminDashboard.jsx
@@ -1,9 +1,60 @@
 import React from "react";
+import { MdCurrencyExchange, MdProductionQuantityLimits } from "react-icons/md";
+import { FaUsers } from "react-icons/fa";
+import { FaCartShopping } from "react-icons/fa6";
 
 const AdminDashboard = () => {
   return (
-    <div>
-      <h1>Admin Dashboard</h1>
+    <div className="px-2 py-5 md:px-7">
+      <div className="grid w-full grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-7">
+        {/* Items */}
+        <div className="flex justify-between items-center p-5 bg-[#fae8e8] rounded-md gap-3">
+          <div className="flex flex-col justify-start items-start text-[#5c5a5a]">
+            <h2 className="text-3xl font-bold">$4356</h2>
+            <span className="font-medium text-md">Total Salse</span>
+          </div>
+
+          <div className="w-[40px] h-[47px] rounded-full bg-[#fa0305] flex justify-center items-center text-xl">
+            <MdCurrencyExchange className="text-[#fae8e8] shadow-lg" />
+          </div>
+        </div>
+        {/* Items */}
+
+        <div className="flex justify-between items-center p-5 bg-[#fde2ff] rounded-md gap-3">
+          <div className="flex flex-col justify-start items-start text-[#5c5a5a]">
+            <h2 className="text-3xl font-bold">100</h2>
+            <span className="font-medium text-md">Products</span>
+          </div>
+
+          <div className="w-[40px] h-[47px] rounded-full bg-[#760077] flex justify-center items-center text-xl">
+            <MdProductionQuantityLimits className="text-[#fae8e8] shadow-lg" />
+          </div>
+        </div>
+        {/* Items */}
+
+        <div className="flex justify-between items-center p-5 bg-[#e9feea] rounded-md gap-3">
+          <div className="flex flex-col justify-start items-start text-[#5c5a5a]">
+            <h2 className="text-3xl font-bold">1000</h2>
+            <span className="font-medium text-md">Sellers</span>
+          </div>
+
+          <div className="w-[40px] h-[47px] rounded-full bg-[#038000] flex justify-center items-center text-xl">
+            <FaUsers className="text-[#fae8e8] shadow-lg" />
+          </div>
+        </div>
+        {/* Items */}
+
+        <div className="flex justify-between items-center p-5 bg-[#ecebff] rounded-md gap-3">
+          <div className="flex flex-col justify-start items-start text-[#5c5a5a]">
+            <h2 className="text-3xl font-bold">20</h2>
+            <span className="font-medium text-md">Orders</span>
+          </div>
+
+          <div className="w-[40px] h-[47px] rounded-full bg-[#0200f8] flex justify-center items-center text-xl">
+            <FaCartShopping className="text-[#fae8e8] shadow-lg" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
